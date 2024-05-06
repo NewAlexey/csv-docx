3f19a072a2aef931561806a91d67939918c53a26
NewAlexey <alexey.krupenia@gmail.com>
2024-05-06 13:09:05 +0300

    feat: add build into main for gh-page

--- src/components/DocCreator/types.ts
+++
@@ -1,2 +0,0 @@
-export type ColumnType = "first" | "second" | "third" | "fourth";
-export type HeaderDataType = { title: string; column: ColumnType };
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import "./style.css";
-import App from "./app.ts";
-
-App.init();
-
-
